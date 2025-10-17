f5f2a63d3bee520b159e0d00652791a1d2e6b6aa
hechar788 <149678618+hechar788@users.noreply.github.com>
2025-10-17 13:41:34 +1300

    Updated nearby-rooms api

--- src/data/blocks/a_block/floor2.ts
+++ src/data/blocks/a_block/floor2.ts
@@ -90,17 +90,7 @@ export const aBlockFloor2Area: Area = {
       directions: {
         forward: { connection: 'a-f2-mid-4' },
         back: { connection: 'a-f2-north-2' }
-      },
-      nearbyRooms: [
-        {
-          roomNumber: 'Unkown',
-          roomType: 'classroom'
-        },
-        {
-          roomNumber: 'Unkown',
-          roomType: 'classroom'
-        }
-      ]
+      }
     },
     {
       id: 'a-f2-mid-4',
@@ -115,16 +105,6 @@ export const aBlockFloor2Area: Area = {
         {
           direction: 'down',
           position: { x: -9.5, y: -0.35, z: -0.25 }
-        }
-      ],
-      nearbyRooms: [
-        {
-          roomNumber: 'Unkown',
-          roomType: 'classroom'
-        },
-        {
-          roomNumber: 'Unkown',
-          roomType: 'classroom'
         }
       ]
     },
